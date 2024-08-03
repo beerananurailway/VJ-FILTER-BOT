c7f9546deaf769b5b23ef0827101967c169ade2d
beerananurailway <138213350+beerananurailway@users.noreply.github.com>
2024-08-03 18:42:47 +0530

    Update info.py
    Signed-off-by: beerananurailway <138213350+beerananurailway@users.noreply.github.com>

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://autofilter3:autofilter3@cluster0.xt1qoi7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://autofilter2:autofilter2@cluster0.82f4tnh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
